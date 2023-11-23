855323963f96ffc201abc60e2ad6de6488edf560
hellum55 <145111412+hellum55@users.noreply.github.com>
2023-11-23 08:24:55 +0100

    Update KNN_Regression.R

--- Fundamentals of modelling (KNN)/KNN_Regression.R
+++ Fundamentals of modelling (KNN)/KNN_Regression.R
@@ -1,8 +1,5 @@
 #########################################################
-# Course: Machine Learning for BI 1 
-# Lecture 1: Fundamentals and KNN alg.
-# Copyright: Ana Alina Tudoran
-# Aarhus University, Fall 2023
+# Course: Machine Learning
 #########################################################
 
 # KNN regression - Application
