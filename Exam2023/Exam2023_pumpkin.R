9f774a40adc237e26f4fb0cd8c0c512195ac2bbd
hellum55 <hellum.christian@gmail.com>
2023-11-27 19:53:57 +0100

    exam

--- Exam2023/Exam2023_pumpkin.R
+++ Exam2023/Exam2023_pumpkin.R
@@ -22,6 +22,7 @@ pumpkin_data <- pumpkin_data %>%
 #Question e:####
 library(visdat)
 library(DataExplorer)
+pumpkin_data[pumpkin_data==""] <- NA
 sum(is.na(pumpkin_data))
 
 # Visually (e.g. plot)
@@ -32,7 +33,7 @@ plot_missing(pumpkin_data)
 #drop these variables.
 
 pumpkin_data <- pumpkin_data %>%
-  select(-c(Trans.Mode, Crop, Storage, Appearance, Condition, Quality, Environment, Grade))
+  select(-c(Trans.Mode, Crop, Storage, Appearance, Condition, Quality, Environment, Grade, Sub.Variety, Unit.of.Sale, Origin.District, Type))
 
 #Checking for missing values again:
 plot_missing(pumpkin_data)
@@ -86,8 +87,8 @@ library(DataExplorer)
 library (GGally)
 
 str(pumpkin_data)
-pairs(pumpkin_data[,c(6:9, 16:18)])
-ggpairs(pumpkin_data, columns = c(6:9, 16:18))
+pairs(pumpkin_data[,c(4:7, 12:14)])
+ggpairs(pumpkin_data, columns = c(4:7, 12:14))
 
 plot_histogram(pumpkin_data)
 
@@ -98,12 +99,60 @@ pumpkin_data %>%
   geom_col(fill = "midnightblue", alpha = 0.7) +
   ylab("Pumpkin Price")
 
+#Question j:####
+#Splitting the data:
+# data split
+library(caret)    # for various ML tasks
+library(recipes)  # for feature engineering tasks
+library(rsample)
 
+set.seed(123)
+split <- initial_split(pumpkin_data, prop = 0.8, strata = "price") 
+pumpkin_train <- training(split)
+pumpkin_test <- testing(split)
 
+pumpkin_recipe <- recipe(price ~ ., data = pumpkin_train) %>%
+  step_impute_knn(all_predictors(), neighbors = 6) %>%
+  step_mutate(Item.Size = ordered(Item.Size, levels = c('sml', 'med', 'med-lge', 'lge', 'xlge', 'jbo', 'exjbo'))) %>%
+  step_integer(all_predictors(), zero_based = F) %>%  
+  #step_dummy(all_nominal(), -all_outcomes(), one_hot = TRUE) %>%
+  step_BoxCox(all_outcomes()) %>% 
+  step_nzv(all_predictors(), -all_outcomes()) %>% 
+  step_center(all_numeric(), -all_outcomes()) %>%
+  step_scale(all_numeric(), -all_outcomes())
 
+prepare <- prep(pumpkin_recipe, training = pumpkin_train)
+prepare
+# bake: apply the recipe to new data (e.g., the training data or future test data) with bake()
+baked_train <- bake(prepare, new_data = pumpkin_train)
+baked_test <- bake(prepare, new_data = pumpkin_test)
+baked_train
+baked_test
 
+#Question k:####
+cv <- trainControl(
+  method = "repeatedcv", 
+  number = 10, 
+  repeats = 1)
 
+# Construct grid of hyperparameter values
+hyper_grid <- expand.grid(k = seq(2, 25, by = 1))
 
+# Tune a knn model using grid search
+pumpkin.knn <- train(
+  pumpkin_recipe, 
+  data = pumpkin_train, 
+  method = "knn", 
+  trControl = cv, 
+  #tuneGrid = hyper_grid,
+  metric = "RMSE"
+)
+pumpkin.knn
+
+# new data (test)
+predictions_reg <- predict(pumpkin.reg, pumpkin_test)
+test_RMSE_reg =sqrt(mean((log(pumpkin_test$price) - predictions_reg)^2))
+test_RMSE_reg
 
 
 
